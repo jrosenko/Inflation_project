42f951a04b94b7a7d0a1363b542915da5d1170ab
jim rosenkoetter <jrosenko@umich.edu>
2021-07-20 17:30:58 -0700

    Update data download to include goods index and services index

--- data_download.py
+++ data_download.py
@@ -6,7 +6,17 @@ from fredapi import Fred
 
 # get data
 # you need to imput your own api key
-fred = Fred(api_key='XXXX')
-cpi = fred.get_series('CPIAUCSL')
+fredkey = 'XXXX'
+fred = Fred(api_key=fredkey)
 
-cpi.to_csv('cpi_index.csv')
+# create dataframe of inflation data series
+df= {}
+
+df['cpi'] = fred.get_series('CPIAUCSL')  #CPI All items index
+df['cpi_goods'] = fred.get_series('CUSR0000SACL1E')  #CPI Commodities Less Food and Energy Commodities
+df['cpi_services'] = fred.get_series('CUSR0000SASLE') #CPI Services Less Energy Services in U.S. City Average
+
+df = pd.DataFrame(df)
+
+# save to file for use downsteam
+df.to_csv('cpi_index.csv')
