b08d2054d6803b9dcbf17d7ea778c322456172ac
jim rosenkoetter <jrosenko@umich.edu>
2021-09-08 19:00:27 -0700

    updated files to download more fields and chart date

--- data_download.py
+++ data_download.py
@@ -6,7 +6,7 @@ from fredapi import Fred
 
 # get data
 # you need to imput your own api key
-fredkey = 'XXXX'
+fredkey = 'xxxx'
 fred = Fred(api_key=fredkey)
 
 # create dataframe of inflation data series
@@ -15,6 +15,8 @@ df= {}
 df['cpi'] = fred.get_series('CPIAUCSL')  #CPI All items index
 df['cpi_goods'] = fred.get_series('CUSR0000SACL1E')  #CPI Commodities Less Food and Energy Commodities
 df['cpi_services'] = fred.get_series('CUSR0000SASLE') #CPI Services Less Energy Services in U.S. City Average
+df['retail_sales'] = fred.get_series('RSAFS') # Advance Retail Sales: Retail Trade and Food Services
+df['avg_hourly_earnings'] = fred.get_series('CES0500000003') # A Average Hourly Earnings of All Employees, Total Private
 
 df = pd.DataFrame(df)
 
